20cc974923b99796e417c31d7e67d00d3abca104
Administrator <Administrator@PC-20160919WKPV>
2017-04-20 14:35:51 +0800

    添加项目文件。

---
+++ Study_Event_And_Delegate/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Study_Event_And_Delegate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            //InitializeComponent();
+            GreetDelegate delegate1 =new GreetDelegate(EnglishGreet);
+            GreetPeople("xX", EnglishGreet);
+            GreetPeople("xx", delegate1);
+        }
+
+        public delegate void GreetDelegate(string name);
+        public void GreetPeople(string name, GreetDelegate MakeGreet) {
+             MakeGreet(name);
+        }
+        public void EnglishGreet(string name) {
+            Console.WriteLine("Good Moring " + name);
+        }
+        public void ChineseGreet(string name) {
+            Console.WriteLine("早上好" + name);
+        }
+    }
+}
---
+++ Study_Event_And_Delegate/StudyEvent01.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Study_Event_And_Delegate
+{
+    public partial class StudyEvent01 : Form
+    {
+        private int temperature;
+        public delegate void BoilHandler(int param);
+        public event BoilHandler BoilEvent;
+
+        public void BoilWater()
+        {
+            for (int i=0; i < 100; i++)
+            {
+                temperature = i;
+                if (temperature > 95)
+                {
+                    if (BoilEvent != null)
+                    {
+                        BoilEvent(temperature);
+                    }
+                }
+            }
+        }
+
+        public StudyEvent01()
+        {
+            InitializeComponent();
+        }
+    }
+}
