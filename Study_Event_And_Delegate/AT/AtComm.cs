85686d5fcaeaf7858447ed45dc6564d50cbbd177
yang yang <yyang20161988@hotmail.com>
2017-04-26 18:30:49 +0800

    ....

--- Study_Event_And_Delegate/AT/AtComm.cs
+++ Study_Event_And_Delegate/AT/AtComm.cs
@@ -20,29 +20,199 @@ namespace Study_Event_And_Delegate.AT
         int startTime; //开始时间
         bool commState; //命令执行结果
         string PortERROR; //串口出现的Error信息
-        //事件
-        public delegate void PortReceviceEventHandler(object sender,PortReceviceEventArgs e);
-        public event PortReceviceEventHandler PortReceviceEvent;//接收数据事件
-        public event EventHandler<AtCommEventArgs> PortSendEvent;//发送数据事件
-        public event EventHandler<AtCommEventArgs> PortTimeOutEvent;
+        public static bool ATE;
+        /// <summary>
+        /// 发送字符串
+        /// </summary>
+        public string SendStr
+        {
+            get
+            {
+                return sendStr;
+            }
 
+            set
+            {
+                sendStr = value;
+                AtCommEventArgs e = new AtCommEventArgs(value);
+                PortSendEvent?.Invoke(this, e);
+            }
+        }
+        /// <summary>
+        /// 接收到的字符串
+        /// </summary>
+        public string ReceviceStr
+        {
+            get
+            {
+                return receviceStr;
+            }
+
+            set
+            {
+                receviceStr = value;
+                AtCommEventArgs e = new AtCommEventArgs(value);
+                PortReceviceEvent?.Invoke(this,e);
+            }
+        }
+        /// <summary>
+        /// 期望字符串
+        /// </summary>
+        public string ExpectStr
+        {
+            get
+            {
+                return expectStr;
+            }
+
+            set
+            {
+                expectStr = value;
+            }
+        }
+        /// <summary>
+        /// 命令成功执行下次执行的字符串
+        /// </summary>
+        public string NextStr
+        {
+            get
+            {
+                return nextStr;
+            }
+
+            set
+            {
+                nextStr = value;
+            }
+        }
+        /// <summary>
+        /// 执行失败执行的字符串
+        /// </summary>
+        public string ElseStr
+        {
+            get
+            {
+                return elseStr;
+            }
+
+            set
+            {
+                elseStr = value;
+            }
+        }
+        /// <summary>
+        /// 发送标识
+        /// </summary>
+        public bool SendFlag
+        {
+            get
+            {
+                return sendFlag;
+            }
+
+            set
+            {
+                sendFlag = value;
+            }
+        }
+        /// <summary>
+        /// 超时标识
+        /// </summary>
+        public bool IsTimeOut
+        {
+            get
+            {
+                return isTimeOut;
+            }
+
+            set
+            {
+                isTimeOut = value;
+            }
+        }
+        /// <summary>
+        /// 超时时间
+        /// </summary>
+        public int TimeOut
+        {
+            get
+            {
+                return timeOut;
+            }
+
+            set
+            {
+                timeOut = value;
+            }
+        }
+        /// <summary>
+        /// 开始时间
+        /// </summary>
+        public int StartTime
+        {
+            get
+            {
+                return startTime;
+            }
+
+            set
+            {
+                startTime = value;
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool CommState
+        {
+            get
+            {
+                return commState;
+            }
+
+            set
+            {
+                commState = value;
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string PortERROR1
+        {
+            get
+            {
+                return PortERROR;
+            }
+
+            set
+            {
+                PortERROR = value;
+            }
+        }
+
+        //事件
+        //public delegate void PortReceviceEventHandler(object sender,PortReceviceEventArgs e);
+        //public event PortReceviceEventHandler PortReceviceEvent;//接收数据事件
+        public event EventHandler<AtCommEventArgs> PortSendEvent;//发送数据事件
+        public event EventHandler<AtCommEventArgs> PortTimeOutEvent;//超时事件
+        public event EventHandler<AtCommEventArgs> PortReceviceEvent;
+
+        //激活超时事件
         public void RaisePortTimeOutEvent(){
-            AtCommEventArgs e = new AtCommEventArgs(isTimeOut);
+            AtCommEventArgs e = new AtCommEventArgs(IsTimeOut);
             PortTimeOutEvent?.Invoke(this, e);
         }
-        public void RaisePoreSendEvent(bool sendFlag)
+        //激活发送事件
+        public void RaisePortSendEvent(bool sendFlag)
         {
             AtCommEventArgs e = new AtCommEventArgs(sendFlag);
             PortSendEvent?.Invoke(this, e); 
         }
-        public class PortReceviceEventArgs : EventArgs
+        public void RaisePortReceviceEvent(byte[] bytes)
         {
-            string receviceStr;
-            public PortReceviceEventArgs(string receviceStr)
-            {
-                this.receviceStr = receviceStr;
-            }
         }
+        //串口事件,赋值和处理
         public class AtCommEventArgs : EventArgs
         {
             bool handle;
@@ -57,13 +227,6 @@ namespace Study_Event_And_Delegate.AT
                 this.handle = sendFlag;
             }
 
-        }
-        public void setReceviceStr(string str) {
-            if (str.Length > 0)
-            {
-                PortReceviceEventArgs e = new PortReceviceEventArgs(str);
-                PortReceviceEvent?.Invoke(this, e);
-            }
         }
         private delegate void longToShort(string[] strs);
         private void setStrs(string[] strs, longToShort DoLongToShort) {
@@ -83,26 +246,27 @@ namespace Study_Event_And_Delegate.AT
 
         }
         private void If3Count(string[] strs) {
-            this.sendStr = strs[0];
-            this.expectStr = strs[1];
-            this.nextStr = strs[2];
+            this.SendStr = strs[0];
+            this.ExpectStr = strs[1];
+            this.NextStr = strs[2];
         }
         private void If4Count(string[] strs)
         {
             If3Count(strs);
-            this.timeOut =int.Parse( strs[3]);
+            this.TimeOut =int.Parse( strs[3]);
         }
 
         public void runComm(SerialPort sp,bool endLine)
         {
             try { 
-            sp.WriteLine(sendStr + (endLine ? "/r/n" : ""));
+            sp.WriteLine(SendStr + (endLine ? "/r/n" : ""));
             }
             catch(Exception E)
             {
-                ;
+                PortERROR1 = E.ToString();
             }
-            RaisePoreSendEvent(true);
+            RaisePortSendEvent(true);
+            
         }
     }
 }
--- Study_Event_And_Delegate/AT/Recive.cs
+++ Study_Event_And_Delegate/AT/Recive.cs
@@ -7,5 +7,73 @@ namespace Study_Event_And_Delegate.AT
 {
     class Recive
     {
+        string[] strs;
+        bool commState;
+        Recive(AtComm atComm)
+        {
+            //解析收到的字符串
+        }
+        Recive(string str, AtComm atComm)
+        {
+            //解析字符串
+            /*
+             * 
+             * 
+             * */
+            string souceStr = atComm.ExpectStr;
+            //截取字符串并放入字符串数组
+            int lines = souceStr.Split('\n').Length;//获取字符串的总行数
+            strs = new string[lines];
+            for (int i = 0; i < lines; i++)
+            {
+                int sLength = souceStr.IndexOf('\n') + 1;
+                string temp = souceStr.Substring(0, sLength);
+                strs[i] = temp;
+                souceStr = souceStr.Remove(0, sLength);
+            }
+            //解析的字符串与期望值对比
+            Commpare(atComm);
+        }
+        enum expectState
+        {
+            S,
+            C,
+            E
+        }
+        private void Commpare(AtComm atComm)
+        {
+            string expectStr = atComm.ExpectStr.Substring(1, atComm.ExpectStr.Length - 1);
+            char expectHead = atComm.ExpectStr.Substring(0, 1).ToCharArray()[0];
+            switch (expectHead)
+            {
+                case (char)expectState.S:
+                    if (strs[0].StartsWith(expectStr))
+                    {
+                        atComm.CommState = true;
+                    }
+                    break;
+                case (char)expectState.C:
+                    string afterS="";
+                    for(int i = 1; i < strs.Length-1; i++)
+                    {
+                        afterS += strs[i];
+                    }
+                    if(afterS.Contains(expectStr))
+                    {
+                        atComm.CommState = true;
+                    }
+                    break;
+                case (char)expectState.E:
+                    if (strs[strs.Length - 1].StartsWith(expectStr))
+                    {
+                        atComm.CommState = true;
+                    }
+                    break;
+                default:
+                    break;
+            }
+                
+
+        }
     }
 }
